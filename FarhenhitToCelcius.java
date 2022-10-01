56d8cb7c837a5a5f88cd9041c11164e929f2a41b
Vinayak Gupta <87488303+tovinayak@users.noreply.github.com>
2022-10-01 19:23:36 +0530

    Add files via upload

---
+++ FarhenhitToCelcius.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class FarhenhitToCelcius {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		float f=s.nextFloat();
+		float c=(float)(5.0/9)*(f-32);
+		System.out.println(c);
+	}
+
+}
